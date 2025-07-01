895977973b353aa14073c7d9af3af50816bfa77b
Tushar Kanta Padhiary <67319069+tushar-kp-10@users.noreply.github.com>
2025-07-01 14:17:53 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ function App() {
 
   return (
     <>
+      <h2>Welcome to my domain</h2>
       <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
